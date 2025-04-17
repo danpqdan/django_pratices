3a9b53810d756bfc44526fa9132909d0e1947f8a
bgmax-daniel <danieltisantos@gmail.com>
2025-04-17 13:26:39 -0300

    [ADD] django setting module

--- projeto_inicial/settings.py
+++ projeto_inicial/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projeto_inicial.settings")
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-wwze4$780m3qpmw4s0*g1*22g3lgdnk4dno&$5_%i(!4u-w^0m"
 
