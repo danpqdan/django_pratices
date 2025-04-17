8283539679145aedb4afb4ff3f90781347660cd0
bgmax-daniel <danieltisantos@gmail.com>
2025-04-17 11:56:36 -0300

    [ADD configurações para realização do deploy em nuvem

--- projeto_inicial/settings.py
+++ projeto_inicial/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-wwze4$780m3qpmw4s0*g1*22g3lgdnk4dno&$5_%i(!4u-w^0m"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["212.85.20.166", "bgmax-tecnologia.com.br"]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Application definition
@@ -141,3 +144,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
