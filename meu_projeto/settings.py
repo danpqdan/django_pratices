99058b2836df2521b6797c9af2d041aecb3397f3
bgmax-daniel <danieltisantos@gmail.com>
2025-04-22 13:50:23 -0300

    [REF] editado settings para configuração do OLS

--- meu_projeto/settings.py
+++ meu_projeto/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$@^!h&ca9cxoohr@4orvyy36ct(hv(-lbsjr!+&xjrty&zudu%'
+SECRET_KEY = "django-insecure-$@^!h&ca9cxoohr@4orvyy36ct(hv(-lbsjr!+&xjrty&zudu%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -19,45 +19,45 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'saudacao',
-    'login',
-    'menu',
-    'clientes',
-    'produtos',
-    'vendas',
-    'usuarios',
-    'relatorios',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "saudacao",
+    "login",
+    "menu",
+    "clientes",
+    "produtos",
+    "vendas",
+    "usuarios",
+    "relatorios",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'meu_projeto.urls'
+ROOT_URLCONF = "meu_projeto.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
@@ -65,39 +65,39 @@ TEMPLATES = [
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'DIRS': [
-            BASE_DIR / 'templates',          
-            BASE_DIR / 'login/templates',    
-            BASE_DIR / 'menu/templates',     
-            BASE_DIR / 'clientes/templates',
-            BASE_DIR / 'produtos/templates',
-            BASE_DIR / 'vendas/templates',
-            BASE_DIR / 'usuarios/templates',
-            BASE_DIR / 'relatorios/templates',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [
+            BASE_DIR / "templates",
+            BASE_DIR / "login/templates",
+            BASE_DIR / "menu/templates",
+            BASE_DIR / "clientes/templates",
+            BASE_DIR / "produtos/templates",
+            BASE_DIR / "vendas/templates",
+            BASE_DIR / "usuarios/templates",
+            BASE_DIR / "relatorios/templates",
         ],
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'meu_projeto.wsgi.application'
+WSGI_APPLICATION = "meu_projeto.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -107,47 +107,47 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 
-#AUTH_USER_MODEL = 'usuarios.Usuario'
+# AUTH_USER_MODEL = 'usuarios.Usuario'
 
 
-LOGIN_REDIRECT_URL = '/menu/'       # Redirecionar para o menu após login
-LOGOUT_REDIRECT_URL = '/login/'     # Redirecionar para a página de login após logout
-LOGIN_URL = '/login/'               # URL da página de login
+LOGIN_REDIRECT_URL = "/menu/"  # Redirecionar para o menu após login
+LOGOUT_REDIRECT_URL = "/login/"  # Redirecionar para a página de login após logout
+LOGIN_URL = "/login/"  # URL da página de login
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'         # Define o idioma padrão como Português Brasil
+LANGUAGE_CODE = "pt-br"  # Define o idioma padrão como Português Brasil
 
-TIME_ZONE = 'America/Sao_Paulo' # Define o fuso horário para São Paulo
+TIME_ZONE = "America/Sao_Paulo"  # Define o fuso horário para São Paulo
 
-USE_I18N = True                 # Ativa a internacionalização (traduções e formatação local)
+USE_I18N = True  # Ativa a internacionalização (traduções e formatação local)
 
-USE_TZ = True                   # Ativa o uso de fuso horário
+USE_TZ = True  # Ativa o uso de fuso horário
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/python/static/"
+STATIC_ROOT = "/usr/local/lsws/Example/html/django_pratices/public/static"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
